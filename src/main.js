e75c06c3a0764b59846310d490872e233fdff48e
Rico P <MailToRico@googlemail.com>
2012-08-14 20:07:55 +0200

    working renderer

--- src/main.js
+++ src/main.js
@@ -7,15 +7,144 @@
 var canvas = document.getElementsByTagName("canvas")[0]; 
 var ctx    = canvas.getContext("2d"); 
 
-GLT.loadmanager.loadFiles({
-	files : ["../map/map1.json"],
-	finished : function(files) {
-		var map = files["../map/map1.json"]; 
+function loadMap(mapname) { 
+	GLT.loadmanager.loadFiles({
+		files : [mapname],
+		finished : function(files) {
+			var mapdata = files[mapname]; 
+			console.log(mapdata.tilewidth); 
+			console.log(mapdata.tileset) ;
 
-		console.log(map.tilewidth); 
+			GLT.loadmanager.loadFiles({
+				files : mapdata.tilesets.map(function(tile) { return tile.image; }), 
+				finished : function(images) {
+					console.log(images); 
+
+					start(mapdata, images); 
+				}
+			}); 
+
+		}
+	}); 
+}
+
+function checkLayerDimension(layers) {
+	var first = layers[0]; 
+	var dim = { width : first.width, height : first.height }; 
+
+	for(var i = 1; i < layers.length; i++) {
+		var layer = layers[i]; 
+		if(layer.width !== dim.width || layer.height !== layer.height) {
+			throw new Error("Size of layer '" + layer.name + "' is different of layer '" + first.name + "."); 
+		}
 	}
-}); 
 
+	return dim; 
+}
+
+
+function mapIndexToTile(id, tilesets, images) {
+	if(id === 0) {
+		return null; 
+	}
+
+	var tiles = tilesets.slice(0); 
+	tiles.sort(function(a,b) { return a.firstgid - b.firstgid; }); 
+
+	var lastSet = null; 
+	var nextSet = null; 
+
+	var i = 0; 
+	do {
+		lastSet = tiles[i]; 
+		i++; 
+		nextSet = tiles[i]; 
+	} while( nextSet && id >= nextSet.firstgid ); 
+
+	var tileset = lastSet; 
+	var offset = id - tileset.firstgid; 
+
+	var width  = (tileset.imagewidth  / tileset.tilewidth)  | 0; 
+	//var height = (tileset.imageheight / tileset.tileheight) | 0; 
+
+	return {
+		image  : images[tileset.image], 
+		offx   : offset % width,
+		offy   : (offset / width) | 0
+	};
+}
+
+function addGrid(dimension, grids, layer, tilesets, images) {
+	var grid = new Array(dimension.width); 
+	for(var x = 0; x < dimension.width; x++) {
+		grid[x] = new Array(dimension.height); 
+	}
+
+	var data = layer.data; 
+	var x = 0, y = 0; 
+	for(var i = 0; i !== data.length; i++) {
+		x = i % dimension.width; 
+		y = (i / dimension.width) | 0; 
+
+		grid[x][y] = mapIndexToTile(data[i], tilesets, images); 
+	}
+
+	grids[layer.name] = grid; 
+}
+
+function start(map, images) {
+	var dimension = checkLayerDimension(map.layers); 
+	var grids = {}; 
+
+	for(var l = 0; l !== map.layers.length; l++) {
+		addGrid(dimension, grids, map.layers[l], map.tilesets, images); 
+	}
+
+	console.log(grids); 
+	map.grids = grids; 
+
+	startGameLoop(map); 
+}
+
+function startGameLoop(map) {
+	var width      = map.width; 
+	var height     = map.height; 
+	var tilewidth  = map.tilewidth; 
+	var tileheight = map.tileheight; 
+	var bg         = map.grids.background; 
+	var obstacles  = map.grids.obstacles; 
+	var objects    = map.grids.objects;   
+	var ceiling    = map.grids.ceiling;   
+
+	var layer = [bg, obstacles, objects, ceiling]; 
+
+	function gameloop(info) {
+		for(var x = 0; x !== map.width; x++) {
+			for(var y = 0; y !== map.height; y++) {
+				for(var l = 0; l !== layer.length; l++) { 
+					var tile = layer[l][x][y]; 
+					if(tile) { 
+						ctx.drawImage(
+							tile.image, 
+							tilewidth * tile.offx, tileheight * tile.offy, 
+							tilewidth, tileheight, 
+							tilewidth * x, tileheight * y, 
+							tilewidth, tileheight
+						); 		
+					}
+				}
+			}
+		}
+
+		GLT.requestGameFrame(gameloop); 
+	}
+
+	GLT.requestGameFrame(gameloop); 
+}
+
+
+
+loadMap("map1.json"); 
 
 }()); 
 
